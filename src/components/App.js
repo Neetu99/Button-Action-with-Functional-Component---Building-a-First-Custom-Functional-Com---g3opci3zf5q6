93049858f6ba6aa1d8c5772026a4ae0d37876eaf
Vedanshi <120268912+Neetu99@users.noreply.github.com>
2023-05-20 17:58:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+ import React, {Component, useState} from "react";
+import "./../styles/App.css";
 
-const App = () => {
-  const [showParagraph, setShowParagraph] = useState(false);
-
-  const handleClick = () => {
-    setShowParagraph(true);
-  };
-
+function App() {
+  let [ele, setFlag] = useState("");
+  
+  function change(){
+    //setFlag(true);
+    setFlag(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+  }
   return (
-    <div>
-      <button id="click" onClick={handleClick}>
-        Click Me
-      </button>
-      {showParagraph && (
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-        </p>
-      )}
+    <>
+    <div id="main">
+      // Do not alter the main div
     </div>
+    {ele}
+    {/* <p id="para" className={flag ? "sho" : "hide"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> */}
+    <button id="click" onClick={change}>show</button>
+    </>
   );
-};
+}
+
 
 export default App;
